2c805401bedc043240580ab9333b26caf0bb4ea2
lucieruin <lucie.wattez@hotmail.fr>
2024-05-14 11:43:12 +0200

    correct else output

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -4,14 +4,13 @@ const request = require('request');
 
 const apiUrl = process.argv[2];
 
-const characterId = '18';
-
 request(apiUrl, (error, response, body) => {
   if (error) {
     console.error(`Error: ${error}`);
   } else {
-    const films = JSON.parse(body).results;
-    const filmsWithWedge = films.filter(film => film.characters.some(`https://swapi.dev/api/people/${characterId}/`));
+    const films = JSON.parse(body);
+    const filmsWithWedge = films.results.filter(film => film.characters.some(character => character.includes('18'))
+    );
     console.log(filmsWithWedge.length);
   }
 });
