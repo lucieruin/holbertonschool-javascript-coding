9132889af028d2ae7c7f29a6fb3ed16bf851063a
lucieruin <lucie.wattez@hotmail.fr>
2024-05-24 10:40:34 +0200

    add condition if user want exit

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,8 +3,13 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (data) => {
-  const name = data.trim();
-  console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing');
-  process.exit();
+  const input = data.trim();
+  if (input === 'exit') {
+    console.log('This important software is now closing');
+    process.exit();
+  } else {
+    console.log(`Your name is: ${input}`);
+    console.log('Welcome to Holberton School, what is your name?');
+    process.exit();
+  }
 });
