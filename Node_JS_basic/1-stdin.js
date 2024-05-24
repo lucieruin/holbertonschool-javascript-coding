dd59990115e061974508432cd2b76f6b12027dcc
lucieruin <lucie.wattez@hotmail.fr>
2024-05-24 11:12:52 +0200

    add a function with this export and a condition for function and add process stdout write

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,18 @@
-console.log('Welcome to Holberton School, what is your name?');
+function yourName() {
+  process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding('utf8');
+  process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', (data) => {
-  const name = data.trim();
-  console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing');
-  process.exit();
-});
+  process.stdin.on('data', (input) => {
+    const name = input.toString();
+    process.stdout.write(`Your name is: ${name}`);
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
+  });
+}
+
+module.exports = yourName;
+
+if (require.main === module) {
+  yourName();
+}
